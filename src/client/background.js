2f1d2866ce4308cc111649153ad687579578bfbd
Cody Palmer <cpalmer@palmtree-studios.net>
2021-01-22 18:41:52 -0600

    hotfix

--- src/client/background.js
+++ src/client/background.js
@@ -7,23 +7,27 @@ const backgroundSkills = document.getElementById('background_skills');
 const backgroundSource = document.getElementById('background_source');
 
 export function updateBackground(data) {
-  //Clear HTML
-  backgroundItems.innerHTML = '';
-  backgroundSkills.innerHTML = '';
-  
-  // Populate HTML
-  backgroundName.innerHTML = `${data.name}`;
-  backgroundText.innerHTML = `${data.text}`;
-  for (let i = 0; i < data.possessions.length; i++) {
-    let item = document.createElement("li");
-    item.innerHTML = `${data.possessions[i]}`;
-    backgroundItems.appendChild(item);
-  }
-  for (let i = 0; i < data.skills.length; i++) {
-    let item = document.createElement("li");
-    item.innerHTML = `${data.skills[i]}`;
-    backgroundSkills.appendChild(item);
-  }
 
-  backgroundSource.innerHTML = `${data.source}`;
+  if (data !== null) {
+    //Clear HTML
+    backgroundItems.innerHTML = '';
+    backgroundSkills.innerHTML = '';
+    
+    // Populate HTML
+    backgroundName.innerHTML = `${data.name}`;
+    backgroundText.innerHTML = `${data.text}`;
+    for (let i = 0; i < data.possessions.length; i++) {
+      let item = document.createElement("li");
+      item.innerHTML = `${data.possessions[i]}`;
+      backgroundItems.appendChild(item);
+    }
+    for (let i = 0; i < data.skills.length; i++) {
+      let item = document.createElement("li");
+      item.innerHTML = `${data.skills[i]}`;
+      backgroundSkills.appendChild(item);
+    }
+
+    backgroundSource.innerHTML = `${data.source}`;
+  }
+  
 }
--- src/client/playerlist.js
+++ src/client/playerlist.js
@@ -7,7 +7,8 @@ export function updatePlayerlist(data) {
   // This is a bit of a hacky way to do this and can get dangerous if you don't escape usernames
   // properly. You would probably use something like React instead if this were a bigger project.
   for (let i = 0; i < data.length; i++) {
-    rows[i + 1].innerHTML = `<td>${escape(data[i].username.slice(0, 15)) || 'Anonymous'}</td><td>${data[i].bg.name || 'Unknown'}</td>`;
+    if (data[i].bg !== null)
+      rows[i + 1].innerHTML = `<td>${escape(data[i].username.slice(0, 15)) || 'Anonymous'}</td><td>${data[i].bg.name || 'Unknown'}</td>`;
   }
   for (let i = data.length; i < 5; i++) {
     rows[i + 1].innerHTML = '<td>-</td><td>-</td>';
